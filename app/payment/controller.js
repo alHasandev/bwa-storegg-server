9e2c37742533d49ecd4f8d4dc4f2c5de2bba0f87
alHasandev <alhasan99dev@gmail.com>
2021-11-26 18:07:40 +0800

    Add: CRUD payment route

---
+++ app/payment/controller.js
@@ -0,0 +1,127 @@
+const Payment = require("./model");
+const Bank = require("../bank/model");
+
+module.exports = {
+  index: async (req, res) => {
+    try {
+      const alert = {
+        message: req.flash("alertMessage"),
+        status: req.flash("alertStatus"),
+      };
+
+      console.log("alert", alert);
+
+      const payments = await Payment.find().populate("banks");
+
+      res.render("admin/payment/view_payment", {
+        payments,
+        alert,
+      });
+    } catch (err) {
+      console.log(err);
+      req.flash("alertMessage", err.message);
+      req.flash("alertStatus", "warning");
+      res.redirect("/payment");
+    }
+  },
+  viewCreate: async (req, res) => {
+    try {
+      const banks = await Bank.find();
+      res.render("admin/payment/create", { banks });
+    } catch (err) {
+      console.log(err);
+    }
+  },
+  actionCreate: async (req, res) => {
+    try {
+      const { type, banks } = req.body;
+
+      const payment = new Payment({
+        type,
+        banks,
+      });
+
+      await payment.save();
+
+      console.log(payment);
+
+      req.flash("alertMessage", `Berhasil tambah pembayaran`);
+      req.flash("alertStatus", "success");
+      res.redirect("/payment");
+    } catch (err) {
+      console.log(err);
+      req.flash("alertMessage", err.message);
+      req.flash("alertStatus", "danger");
+      res.redirect("/payment");
+    }
+  },
+  viewEdit: async (req, res) => {
+    try {
+      const payment = await Payment.findOne({ _id: req.params.id });
+      const banks = await Bank.find();
+
+      console.log(payment);
+      res.render("admin/payment/edit", { payment, banks });
+    } catch (err) {
+      console.log(err);
+      req.flash("alertMessage", err.message);
+      req.flash("alertStatus", "warning");
+      res.redirect("/payment");
+    }
+  },
+  actionEdit: async (req, res) => {
+    try {
+      const { type, banks } = req.body;
+      const payment = await Payment.findOne({ _id: req.params.id });
+      payment.type = type;
+      payment.banks = banks;
+
+      await payment.save();
+
+      req.flash("alertMessage", `Berhasil ubah pembayaran`);
+      req.flash("alertStatus", "success");
+
+      res.redirect("/payment");
+    } catch (err) {
+      console.log(err);
+      req.flash("alertMessage", err.message);
+      req.flash("alertStatus", "danger");
+      res.redirect("/payment");
+    }
+  },
+  actionDelete: async (req, res) => {
+    try {
+      // Delete payment data by id
+      const condition = { _id: req.params.id };
+      result = await Payment.deleteOne(condition);
+
+      // Redirect to page with message
+      req.flash("alertMessage", `Berhasil hapus pembayaran`);
+      req.flash("alertStatus", `success`);
+      res.redirect("/payment");
+    } catch (err) {
+      console.log(err);
+      req.flash("alertMessage", err.message);
+      req.flash("alertStatus", "danger");
+      res.redirect("/payment");
+    }
+  },
+  actionStatus: async (req, res) => {
+    try {
+      // Find payment by id and update it's status
+      const payment = await Payment.findOne({ _id: req.params.id });
+      payment.status = req.body.status;
+      await payment.save();
+
+      // Redirect message to page
+      req.flash("alertMessage", `Berhasil ubah status`);
+      req.flash("alertStatus", "success");
+      res.redirect("/payment");
+    } catch (err) {
+      console.log(err);
+      req.flash("alertMessage", err.message);
+      req.flash("alertStatus", "danger");
+      res.redirect("/payment");
+    }
+  },
+};
---
+++ app/payment/model.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const ObjectId = mongoose.Schema.Types.ObjectId;
+
+const paymentSchema = mongoose.Schema({
+  type: {
+    type: String,
+    required: [true, "Tipe pembayaran harus diisi"],
+  },
+  status: {
+    type: String,
+    enum: ["Y", "N"],
+    default: "Y",
+  },
+  banks: {
+    type: [ObjectId],
+    ref: "bank",
+  },
+});
+
+const Payment = mongoose.model("payment", paymentSchema);
+
+module.exports = Payment;
