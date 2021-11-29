71e4927afd7d7b8aebc84e4834c391bb0232ca98
alHasandev <alhasan99dev@gmail.com>
2021-11-29 15:59:55 +0800

    Fix: Refactory file helper function
    - deleteFile: separate path and filename params
    - uploadImage: return promise

--- helpers/file.js
+++ helpers/file.js
@@ -2,9 +2,18 @@ const path = require("path");
 const fs = require("fs");
 const config = require("../config");
 
-function deleteFile(path) {
-  if (fs.existsSync(path)) {
-    fs.unlinkSync(path);
+/**
+ *
+ * @param {String} path path of file
+ * @param {String} filename provide filename or just left it empty if you just want to use path paramater
+ * @returns {Boolean} (true) if delete success, (false) if delete fail
+ */
+function deleteFile(path, filename) {
+  if (filename) path = `${path}/${filename}`;
+  const fullPath = `${config.rootPath}/${path}`;
+
+  if (fs.existsSync(fullPath)) {
+    fs.unlinkSync(fullPath);
     return true;
   }
 
@@ -16,26 +25,32 @@ function deleteFile(path) {
  * @param {String} dir directory name
  * @param {Blob} file image file
  * @param {Function} cb callback function
+ * @returns {Promise} resolve(filename), reject(error)
  */
 function uploadImage(dir, file, cb) {
-  try {
-    console.log("Upload Image!");
-    let tmp_path = file.path;
-    let fileNames = file.originalname.split(".");
-    let originalExt = fileNames[fileNames.length - 1];
-    let filename = file.filename + "." + originalExt;
-    let target_path = path.resolve(config.rootPath, `${dir}/${filename}`);
+  return new Promise((resolve, reject) => {
+    try {
+      console.log("Upload Image!");
+      let tmp_path = file.path;
+      let fileNames = file.originalname.split(".");
+      let originalExt = fileNames[fileNames.length - 1];
+      let filename = file.filename + "." + originalExt;
+      let target_path = path.resolve(config.rootPath, `${dir}/${filename}`);
 
-    const src = fs.createReadStream(tmp_path);
-    const dest = fs.createWriteStream(target_path);
+      const src = fs.createReadStream(tmp_path);
+      const dest = fs.createWriteStream(target_path);
 
-    src.pipe(dest);
-    if (typeof cb === "function") {
-      src.on("end", () => cb(filename));
+      src.pipe(dest);
+      src.on("end", () => {
+        if (typeof cb === "function") cb(filename);
+        resolve(filename);
+      });
+
+      // resolve(filename);
+    } catch (err) {
+      reject(err);
     }
-  } catch (err) {
-    console.log(err);
-  }
+  });
 }
 
 module.exports = {
