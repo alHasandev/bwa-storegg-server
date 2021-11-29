8ffa17b60694a040650f40621de509fcd4762641
alHasandev <alhasan99dev@gmail.com>
2021-11-29 09:59:41 +0800

    Fix: Show alert on dashboard

--- app/dashboard/controller.js
+++ app/dashboard/controller.js
@@ -1,8 +1,12 @@
 module.exports = {
   index: async (req, res) => {
     try {
+      const alert = {
+        message: req.flash("alertMessage"),
+        status: req.flash("alertStatus"),
+      };
       console.log("user session", req.session.user);
-      res.render("index", { user: req.session.user });
+      res.render("index", { alert, user: req.session.user });
     } catch (err) {
       console.log(err);
     }
