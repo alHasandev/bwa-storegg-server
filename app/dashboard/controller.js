471c4ecd0c6a289f166bc420cd6266a2a9856683
alHasandev <alhasan99dev@gmail.com>
2021-11-29 11:38:18 +0800

    Fix: Move dashboard view to dashboard folder and show stat box value

--- app/dashboard/controller.js
+++ app/dashboard/controller.js
@@ -1,12 +1,31 @@
+const Transaction = require("../transaction/model");
+const Voucher = require("../voucher/model");
+const Category = require("../category/model");
+const Player = require("../player/model");
+
 module.exports = {
   index: async (req, res) => {
     try {
+      const transactionCount = await Transaction.countDocuments();
+      const voucherCount = await Voucher.countDocuments();
+      const categoryCount = await Category.countDocuments();
+      const playerCount = await Player.countDocuments();
+
       const alert = {
         message: req.flash("alertMessage"),
         status: req.flash("alertStatus"),
       };
       console.log("user session", req.session.user);
-      res.render("index", { alert, user: req.session.user });
+      res.render("admin/dashboard/view_dashboard", {
+        alert,
+        user: req.session.user,
+        count: {
+          transaction: transactionCount,
+          voucher: voucherCount,
+          cathegory: categoryCount,
+          player: playerCount,
+        },
+      });
     } catch (err) {
       console.log(err);
     }
