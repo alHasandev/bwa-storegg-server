e62e8d80cf8ebcbb2bffd578ccea544f2da0a477
alHasandev <alhasan99dev@gmail.com>
2021-11-29 14:05:22 +0800

    Fix: auto format prettier kick in

--- app/nominal/model.js
+++ app/nominal/model.js
@@ -1,20 +1,22 @@
 const mongoose = require("mongoose");
 
-const nominalSchema = mongoose.Schema({
-  coinQuantity: {
-    type: Number,
-    default: 0,
+const nominalSchema = mongoose.Schema(
+  {
+    coinQuantity: {
+      type: Number,
+      default: 0,
+    },
+    coinName: {
+      type: String,
+      required: [true, "Nama koin harus diisi"],
+    },
+    price: {
+      type: Number,
+      default: 0,
+    },
   },
-  coinName: {
-    type: String,
-    required: [true, "Nama koin harus diisi"],
-  },
-  price: {
-    type: Number,
-    default: 0,
-  },
-},
-{ timestamps: true });
+  { timestamps: true }
+);
 
 const Nominal = mongoose.model("nominal", nominalSchema);
 
